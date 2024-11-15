2e7cbf9c7ab83130a1cb61b178f4bf505267f936
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-15 09:50:13 +0000

    Terminado tarjeta y funcionalidad show more and less y expanded

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,6 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +8,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -47,7 +45,53 @@ fun Inicio(changeShowInicio: () -> Unit){
 
 @Composable
 fun SegundaPantalla(){
+    val textos: MutableList<String> = mutableListOf(
+        "Hola, qué tal?",
+        "Me llamo Lucas",
+        "Me gusta programar",
+        "Adoro la Tortilla"
+    )
+    textos.forEach{ texto ->
+        Tarjeta(texto)
+    }
+}
 
+@Composable
+fun Tarjeta(texto: String = ""){
+    var expanded by remember { mutableStateOf(0.dp) }
+    var isExpanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        backgroundColor = Color(0xffdee3ff),
+        elevation = 2.dp,
+        shape = MaterialTheme.shapes.small,
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = expanded)
+        ) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = texto
+            )
+            Button(
+                onClick = {
+                    if (isExpanded) {
+                        expanded = 0.dp
+                        isExpanded = !isExpanded
+                    } else{
+                        expanded = 120.dp
+                        isExpanded = !isExpanded
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xff9b88f6))){
+                Text(
+                    text = (if (isExpanded) "Show less" else "Show more"),
+                    color = Color.White
+                )
+            }
+        }
+    }
 }
 
 fun main() = application {
