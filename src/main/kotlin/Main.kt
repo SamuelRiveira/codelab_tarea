c0194aa117ef4a3e4feb5133396441720553eec5
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-15 09:06:50 +0000

    Cambio correcto entre ventanas con funcion lambda

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -8,6 +10,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -15,20 +19,34 @@ import androidx.compose.ui.window.application
 @Preview
 fun App() {
     MaterialTheme {
-        Inicio()
+        var showInicio by remember { mutableStateOf(true) }
+        if (showInicio) {
+            Inicio(changeShowInicio = {showInicio = false})
+        } else {
+            SegundaPantalla()
+        }
     }
 }
 
 @Composable
-fun Inicio(){
-    Column {
+fun Inicio(changeShowInicio: () -> Unit){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
         Text("Esta es la pantalla de inicio")
         Button(
-            onClick = {}
+            onClick = changeShowInicio
         ){
             Text(text = "Continuar")
         }
     }
+
+}
+
+@Composable
+fun SegundaPantalla(){
 
 }
 
